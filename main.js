737e62288fc81e4f3045166285f1432d5dd9bdb9
Guillem Miró <guillemmiro97@gmail.com>
2022-12-09 20:07:16 +0100

    Connection to database with dotenv file.

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-console.log("hello world");
+//connect to mongodb database
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config();
+
+async function main() {
+    const uri = process.env.MONGO_URI;
+    const client = new MongoClient(uri);
+
+    try {
+        await client.connect();
+        await listDatabases(client);
+    }
+    catch (e) {
+        console.error(e);
+    }
+    finally {
+        await client.close();
+    }
+}
+
+async function listDatabases(client){
+    databasesList = await client.db().admin().listDatabases();
+ 
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+main().catch(console.error);
